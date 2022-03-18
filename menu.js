ba44af864eb7f7f41961881aff400963477a6226
Wet Takis <77040285+test1374@users.noreply.github.com>
2022-03-17 23:03:07 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,12 +3,18 @@ const zx = xz => {
     logotransparent: null,
     wettakis: null,
   };
+  let fonts = {
+    menu: null,
+  };
   let elements = [];
+  xz.preload = function() {};
   xz.setup = function() {
+    fonts.menu = xz.loadFont('SourceSansPro-Black.ttf');
     assets.logotransparent = xz.loadImage('Brutal League-logos_transparent.png');
     assets.wettakis = xz.loadImage('Wet Takis Studios-logos_transparent.png');
     let cnv = xz.createCanvas(xz.windowWidth, xz.windowHeight);
     xz.imageMode(xz.CENTER);
+    xz.textAlign(xz.CENTER, xz.CENTER);
     cnv.position(0, 0, 'fixed');
     elements[0] = xz.createInput('');
     elements[0].position(xz.width / 2 - xz.width * 0.15, xz.height / 2);
@@ -29,6 +35,9 @@ const zx = xz => {
     }
     if(xz.frameCount >= 400 && xz.getItem('alpha') != true) {
       xz.background(255);
+      xz.fill(0);
+      xz.textFont(fonts.menu, xz.width / 17);
+      xz.text('Alpha Tester? Copy+Paste the code given to you in this box.', xz.width / 10, xz.height / 3.2, xz.width / 1.2);
       elements[0].show();
       if(elements[0].value() == window.btoa('dbcakdjhcjsow')) {
         xz.storeItem('alpha', true);
